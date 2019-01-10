4477ae9685e55ea491c72d260cb979fdb764fce7
Ali Mehdi <salimehdi144@gmail.com>
2019-01-10 22:54:39 +0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -9,3 +9,11 @@ a = pd.read_csv("./set-a/132539.txt")
 HR = np.array(a[a.Parameter == 'HR'].Value)
 RR = np.array(a[a.Parameter == 'RespRate'].Value)
 
+# taking care of missing data
+for x in range(37,42):
+ HR = np.append(HR,HR.mean())
+
+# For Plot
+x = HR
+y = RR
+

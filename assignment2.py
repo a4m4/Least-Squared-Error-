ab69642c0628f0a5e2395e07f76306159529034a
Ali Mehdi <salimehdi144@gmail.com>
2019-01-10 23:12:25 +0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
 from sklearn import datasets, linear_model
 from numpy.polynomial.polynomial import polyfit
 
@@ -17,3 +18,22 @@ for x in range(37,42):
 x = HR
 y = RR
 
+# Plot outputs
+plt.scatter(x,y, color='blue')
+plt.title('SCATTER PLOT')
+plt.xlabel('HR')
+plt.ylabel('RESP RATE')
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
+# Fit with polyfit
+b, m = polyfit(x, y, 1)
+
+plt.figure(figsize=(6,4))
+plt.plot(x, y, '.')
+plt.plot(x, b + m * x, color='yellow')
+plt.title('LSE FIT')
+plt.xlabel(r'$HR$')
+plt.ylabel(r'$RespRate$')
+plt.show()
